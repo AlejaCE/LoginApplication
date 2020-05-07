82c0371dc5de05a1178abccd5832920243723f41
aleja <pacadenae@unal.edu.co>
2020-05-06 22:26:00 -0500

    Lab2Realizado
    Lab2Realizado

--- src/Control/ValidarLogin.java
+++ src/Control/ValidarLogin.java
@@ -5,7 +5,9 @@
  */
 package Control;
 
+import Entidad.Sistema;
 import Entidad.Usuario;
+import Frontera.FramePrincipal;
 
 /**
  *
@@ -13,19 +15,33 @@ import Entidad.Usuario;
  */
 public class ValidarLogin {
 
+    private Sistema sistema = FramePrincipal.sistema;
+    
     public ValidarLogin() {
     }
     
     public String verificarLogin (Usuario usuario){
-        return null;
+        if(!verificarLongitudNombre(usuario.getNombre())){
+            return("Longitud de nombre incorrecta");
+        }
+        if(!verificarLongitudPassword(usuario.getPassword())){
+            return("Longitud de contraseña incorrecta");
+        }
+        for(Usuario u: sistema.getUsuarios()){
+            if(u.getNombre().equals(usuario.getNombre())&&
+               u.getPassword().equals(usuario.getPassword())){
+                return("Bienvenido");
+            }
+        }
+        return ("Datos incorrectos");
     }
     
-    public boolean verificarLogitudNombre (String nombre){
-        return true;
+    public boolean verificarLongitudNombre (String nombre){
+        return (nombre.length() > 1 && nombre.length() <= 6);
     }
     
-    public boolean verificarLogitudPassword (String password){
-        return true;
+    public boolean verificarLongitudPassword (String password){
+        return (password.length() > 3 && password.length() <= 6);
     }
     
 }
